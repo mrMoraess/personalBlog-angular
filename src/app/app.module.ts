9eb5ccdeb0f6789096479f756c20c1899a57f319
mrMoraess <matheusrodriguesde347@gmail.com>
2024-02-01 21:31:19 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainCardsComponent } from './components/main-cards/main-cards.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
+import { SectionsComponent } from './components/sections/sections.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCardsComponent,
+    UserInfoComponent,
+    ProjectsSectionComponent,
+    SectionsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
